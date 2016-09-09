2ff6eaa87a3db4b1dfc8bb92fb8ce013b8256f7e
Arda Mavi <ardamavi2@gmail.com>
2016-09-09 18:04:33 +0300

    Kullanıcı Arayüzü Güncellendi

--- sosyalAg/mavi/admin.py
+++ sosyalAg/mavi/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from .models import Post
 from .models import guncelYazi
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'first_name',
+        'last_name',
+        'text'
+    ]
+    search_fields = [
+        'name',
+        'first_name',
+        'last_name',
+        'text'
+    ]
 
-admin.site.register(Post)
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(guncelYazi)
